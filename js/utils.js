2c318c582bb74cb155c7587681334a8feb9b0d19
Hasan Karaman <hk@hasankaraman.dev>
2022-06-23 02:51:28 +0300

    style: Computed coordinates will be shown as DMS as well.

--- js/utils.js
+++ js/utils.js
@@ -4,3 +4,10 @@ export function getEllipsoidParams(selection)  {
     document.getElementById('iSemiMajor').value = kEllipsoids.get(selection.value).a;
     document.getElementById('iSemiMinor').value = kEllipsoids.get(selection.value).b;
 }
+
+export function decimalToDMS(angle) {
+    var d = Math.floor(angle);
+    var m = Math.floor((angle - d) * 60);
+    var s = ((angle - d) * 60 - m) * 60;
+    return '(' + d + '\u00B0 ' + m + '\' ' + s.toFixed(6) + '")';
+}
